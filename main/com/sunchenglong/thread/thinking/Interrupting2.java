aa216d8e9be056a8692c903269a760727569c228
suncl <suncl@ios.ac.cn>
2016-08-01 11:32:21 +0800

    ADD: add NIO and Interrupting

---
+++ main/com/sunchenglong/thread/thinking/Interrupting2.java
@@ -0,0 +1,45 @@
+package com.sunchenglong.thread.thinking;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by Administrator on 2016/8/1.
+ */
+class BlockedMutex {
+    private Lock lock = new ReentrantLock();
+
+    public BlockedMutex() {
+        lock.lock();
+    }
+
+    public void f() {
+        try {
+            lock.lockInterruptibly();
+            System.out.println("lock acquired in f()");
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted from lock acquisition in f()");
+        }
+    }
+}
+
+class Blocked2 implements Runnable {
+    BlockedMutex blockedMutex = new BlockedMutex();
+
+    public void run() {
+        System.out.println("Waiting for f() in BlockedMutex()");
+        blockedMutex.f();
+        System.out.println("Broken out of blocked call");
+    }
+}
+
+public class Interrupting2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Blocked2());
+        thread.start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Issuing t.interrupt()");
+        thread.interrupt();
+    }
+}
