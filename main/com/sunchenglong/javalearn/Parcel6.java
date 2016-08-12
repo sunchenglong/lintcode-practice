4b0dd08e171d8167daca5d3f2aac2df297a32ff3
suncl <suncl@ios.ac.cn>
2016-08-12 10:52:01 +0800

    ADD: parcel6

---
+++ main/com/sunchenglong/javalearn/Parcel6.java
@@ -0,0 +1,31 @@
+package com.sunchenglong.javalearn;
+
+/**
+ * Created by Chenglong Sun on 2016/8/12.
+ */
+public class Parcel6 {
+    private void internalTracking(boolean b) {
+        if(b) {
+            class TrackingSlip {
+                private String id;
+                TrackingSlip(String id) {
+                    this.id = id;
+                }
+                String getSlip() {
+                    return id;
+                }
+            }
+            TrackingSlip trackingSlip = new TrackingSlip("Slip!");
+            String s = trackingSlip.getSlip();
+            System.out.println(s);
+        }
+    }
+    public void track() {
+        internalTracking(true);
+    }
+
+    public static void main(String[] args) {
+        Parcel6 parcel6 = new Parcel6();
+        parcel6.track();
+    }
+}
