1f19a8da7b1eec931363257a3681c6713a72e86e
suncl <suncl@ios.ac.cn>
2016-09-26 10:39:10 +0800

    FIX: ArrayMaker

--- main/com/sunchenglong/javalearn/ArrayMaker.java
+++ main/com/sunchenglong/javalearn/ArrayMaker.java
@@ -1,6 +1,8 @@
 package com.sunchenglong.javalearn;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -18,9 +20,15 @@ public class ArrayMaker<T> {
         return ((T[]) Array.newInstance(kind, size));
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         ArrayMaker<String> stringArrayMaker = new ArrayMaker<String>(String.class);
         String[] stringArray = stringArrayMaker.create(5);
         System.out.println(Arrays.toString(stringArray));
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(1);
+        arrayList.getClass().getMethod("add", Object.class).invoke(arrayList,"asd");
+        for(int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
     }
 }
