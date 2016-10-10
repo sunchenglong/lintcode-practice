35c2ff5f14ffff76d58b5d8a89f6f6663dedb6c2
suncl <suncl@ios.ac.cn>
2016-10-10 18:02:59 +0800

    ADD: first LamdaPractice

---
+++ main/com/sunchenglong/javalearn/LamdaPractice.java
@@ -0,0 +1,42 @@
+package com.sunchenglong.javalearn;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Chenglong Sun on 2016/10/10.
+ */
+public class LamdaPractice {
+
+
+    public static void originFun() {
+        List<BigDecimal> prices = Arrays.asList(
+                new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+                new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+                new BigDecimal("45"), new BigDecimal("12"));
+        BigDecimal totalOfDiscountedPrices = BigDecimal.ZERO;
+        for (BigDecimal price : prices) {
+            if (price.compareTo(BigDecimal.valueOf(20)) > 0)
+                totalOfDiscountedPrices = totalOfDiscountedPrices.add(price.multiply(BigDecimal.valueOf(0.9)));
+        }
+        System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
+    }
+
+    public static void lamdaFun() {
+        List<BigDecimal> prices = Arrays.asList(
+                new BigDecimal("10"), new BigDecimal("30"), new BigDecimal("17"),
+                new BigDecimal("20"), new BigDecimal("15"), new BigDecimal("18"),
+                new BigDecimal("45"), new BigDecimal("12"));
+        BigDecimal totalOfDiscountedPrices = prices.stream()
+                .filter(price -> price.compareTo(BigDecimal.valueOf(20)) > 0)
+                .map(price -> price.multiply(BigDecimal.valueOf(0.9)))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        System.out.println("Total of discounted prices: " + totalOfDiscountedPrices);
+    }
+
+    public static void main(String[] args) {
+        originFun();
+        lamdaFun();
+    }
+}
---
+++ main/com/sunchenglong/leetcode/BinaryWatch401.java
@@ -0,0 +1,13 @@
+package com.sunchenglong.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by Chenglong Sun on 2016/10/10.
+ */
+//Todo unfinished
+public class BinaryWatch401 {
+    public List<String> readBinaryWatch(int num) {
+        return null;
+    }
+}
