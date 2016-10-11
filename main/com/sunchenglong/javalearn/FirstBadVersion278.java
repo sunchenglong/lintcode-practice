d81816514d8994a1df212d7882e2abd62ab02da0
suncl <suncl@ios.ac.cn>
2016-10-11 17:57:23 +0800

    ADD: 278

---
+++ main/com/sunchenglong/javalearn/FirstBadVersion278.java
@@ -0,0 +1,26 @@
+package com.sunchenglong.javalearn;
+
+/**
+ * Created by Chenglong Sun on 2016/10/11.
+ */
+public class FirstBadVersion278 extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int media = left + (right - left) / 2;
+            if (isBadVersion(media)) right = media - 1;
+            else {
+                left = media + 1;
+            }
+        }
+        if (isBadVersion(right)) return right;
+        return right + 1;
+    }
+}
+
+class VersionControl {
+    boolean isBadVersion(int version) {
+        return false;
+    }
+}
