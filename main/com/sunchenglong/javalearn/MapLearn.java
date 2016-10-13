f632890447509d135cb3d8bed0c3040daf80c1f6
suncl <suncl@ios.ac.cn>
2016-10-13 23:36:09 +0800

    ADD: mapLearn and LRU cache

---
+++ main/com/sunchenglong/javalearn/MapLearn.java
@@ -0,0 +1,14 @@
+package com.sunchenglong.javalearn;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * Created by Chenglong Sun on 2016/10/13.
+ */
+public class MapLearn {
+    public static void main(String[] args) {
+        Map<Integer,Integer> map= new Hashtable<>();
+
+    }
+}
---
+++ main/com/sunchenglong/leetcode/LRUCache146.java
@@ -0,0 +1,27 @@
+package com.sunchenglong.leetcode;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+/**
+ * Created by Chenglong Sun on 2016/10/13.
+ */
+public class LRUCache146 {
+}
+
+class LRUCache {
+
+    private HashMap<Integer, Integer> hashMap;
+
+    public LRUCache(int capacity) {
+        hashMap = new LinkedHashMap<>(capacity);
+    }
+
+    public int get(int key) {
+        return 0;
+    }
+
+    public void set(int key, int value) {
+
+    }
+}
