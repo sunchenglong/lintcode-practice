731405e007e1e67a7e2af192a43912f8620463a4
suncl <suncl@ios.ac.cn>
2016-10-16 12:50:30 +0800

    ADD: netty server

---
+++ main/com/sunchenglong/javalearn/NettyServer.java
@@ -0,0 +1,33 @@
+package com.sunchenglong.javalearn;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by Chenlong Sun on 2016/10/15.
+ */
+public class NettyServer {
+
+    /**
+     * Handles a server-side channel.
+     */
+    public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+            // Discard the received data silently.
+            ((ByteBuf) msg).release(); // (3)
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+            // Close the connection when an exception is raised.
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+}
