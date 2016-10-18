44b7e31a8be150a570c90c228d6657655090b003
suncl <suncl@ios.ac.cn>
2016-10-18 17:13:29 +0800

    ADD: 20

---
+++ main/com/sunchenglong/javalearn/ValidParentheses20.java
@@ -0,0 +1,30 @@
+package com.sunchenglong.javalearn;
+
+import java.util.Stack;
+
+/**
+ * Created by Chenglong Sun on 2016/10/18.
+ */
+public class ValidParentheses20 {
+    public boolean isValid(String s) {
+        String left = "({[";
+        String right = ")}]";
+        Stack<Character> stack = new Stack<>();
+        int length = s.length();
+        if (length <= 0) return true;
+        if (length % 2 == 1) return false;
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (left.indexOf(c) > -1) {
+                stack.push(c);
+            } else {
+                if (!stack.isEmpty()) {
+                    if (left.indexOf(stack.peek()) == right.indexOf(c)) {
+                        stack.pop();
+                    } else return false;
+                } else return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
