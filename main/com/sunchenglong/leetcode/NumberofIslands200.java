16686972a52df1fb1549e693cbd389f2d68d193f
suncl <suncl@ios.ac.cn>
2016-10-24 20:09:09 +0800

    ADD: 200

---
+++ main/com/sunchenglong/leetcode/NumberofIslands200.java
@@ -0,0 +1,29 @@
+package com.sunchenglong.leetcode;
+
+/**
+ * Created by Chenglong Sun on 2016/10/24.
+ */
+public class NumberofIslands200 {
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    result++;
+                    dfsFill(grid, i, j);
+                }
+            }
+        }
+        return result;
+    }
+
+    private void dfsFill(char[][] grid, int i, int j) {
+        if (i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] == '1') {
+            grid[i][j] = '0';
+            dfsFill(grid, i + 1, j);
+            dfsFill(grid, i - 1, j);
+            dfsFill(grid, i, j + 1);
+            dfsFill(grid, i, j - 1);
+        }
+    }
+}
