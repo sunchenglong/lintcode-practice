3bbc076586e3b0aa44be14071a25d6da93a72e70
suncl <suncl@ios.ac.cn>
2016-10-28 23:39:03 +0800

    ADD: 62

--- main/com/sunchenglong/leetcode/UniquePaths62.java
+++ main/com/sunchenglong/leetcode/UniquePaths62.java
@@ -5,7 +5,13 @@ package com.sunchenglong.leetcode;
  */
 public class UniquePaths62 {
     public int uniquePaths(int m, int n) {
-        int[][] f = new int[m][n];
-        return 0;
+        int[] f = new int[n];
+        f[0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[j] = f[j - 1] + f[j];
+            }
+        }
+        return f[n - 1];
     }
 }
