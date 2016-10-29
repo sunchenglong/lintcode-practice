fb4f9d936c355021d0c6e003b699559b9d8b42c6
suncl <suncl@ios.ac.cn>
2016-10-30 00:24:31 +0800

    ADD: 52

--- main/com/sunchenglong/leetcode/NQueensII52.java
+++ main/com/sunchenglong/leetcode/NQueensII52.java
@@ -6,37 +6,29 @@ import java.util.List;
 /**
  * Created by Chenglong Sun on 2016/10/29.
  */
-public class NQueens51 {
-    public List<List<String>> solveNQueens(int n) {
+public class NQueensII52 {
+
+    public int totalNQueens(int n) {
         List<Integer> taken = new ArrayList<>();
-        List<List<String>> result = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             taken.add(i, -1);
         }
-        getNQueens(result, 0, n, taken);
-        return result;
+        return getNQueens(taken, 0, n);
     }
 
-    private void getNQueens(List<List<String>> result, int level, int n, List<Integer> taken) {
+    private int getNQueens(List<Integer> taken, int level, int n) {
         if (level == n) {
-            List<String> board = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                StringBuilder s = new StringBuilder();
-                for (int j = 0; j < n; j++) {
-                    s.append(".");
-                }
-                s.setCharAt(taken.get(i), 'Q');
-                board.add(s.toString());
-            }
-            result.add(board);
+            return 1;
         } else {
+            int ways = 0;
             for (int i = 0; i < n; i++) {
                 if (isValid(taken, level, i)) {
                     taken.set(level, i);
-                    getNQueens(result, level + 1, n, taken);
+                    ways += getNQueens(taken, level + 1, n);
                     taken.set(level, -1);
                 }
             }
+            return ways;
         }
     }
 
@@ -49,4 +41,7 @@ public class NQueens51 {
         return true;
     }
 
+    public static void main(String[] args) {
+        System.out.println(new NQueensII52().totalNQueens(4));
+    }
 }
