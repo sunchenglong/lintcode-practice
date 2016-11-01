356be069163acabc7ee8d24001f824766c210a50
suncl <suncl@ios.ac.cn>
2016-11-01 20:46:06 +0800

    ADD: 140

--- main/com/sunchenglong/leetcode/WordBreakII140.java
+++ main/com/sunchenglong/leetcode/WordBreakII140.java
@@ -7,51 +7,39 @@ import java.util.*;
  * Created by Chenglong Sun on 2016/10/17.
  */
 
-//Todo Unfinished
 public class WordBreakII140 {
-    Stack<String> path;
-    List<String> result;
-
     public List<String> wordBreak(String s, Set<String> wordDict) {
-        int length = s.length();
-        if (length <= 0) return null;
-        boolean[] f = new boolean[length + 1];
-        f[0] = true;
-        boolean[][] prev = new boolean[length + 1][length + 1];
-        for (int i = 1; i <= length; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (f[j] && wordDict.contains(s.substring(j, i))) {
-                    f[i] = true;
-                    prev[i][j] = true;
+        return dfsSolver(s, new HashMap<>(), wordDict);
+    }
+
+    private List<String> dfsSolver(String s, HashMap<String, LinkedList<String>> map, Set<String> wordDict) {
+        if (map.containsKey(s)) {
+            return map.get(s);
+        }
+        LinkedList<String> result = new LinkedList<>();
+        if (s.length() == 0) {
+            result.add("");
+            return result;
+        }
+        for (String word : wordDict) {
+            if (s.startsWith(word)) {
+                List<String> sublist = dfsSolver(s.substring(word.length()), map, wordDict);
+                for (String sub : sublist) {
+                    result.add(word + (sub.isEmpty() ? "" : " ") + sub);
                 }
             }
         }
-        if (f[length]) {
-            path = new Stack<>();
-            result = new ArrayList<>();
-            dfs(s, prev, length, path, result);
-            return result;
-        } else {
-            return null;
-        }
+        map.put(s, result);
+        return result;
     }
 
-    public void dfs(String s, boolean[][] prev, int cur, Stack<String> path, List<String> result) {
-        if (cur == 0) {
-            StringBuilder stringBuilder = new StringBuilder("");
-            for (String str : path) {
-                stringBuilder.append(str).append(" ");
-            }
-            stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), "");
-            result.add(stringBuilder.toString());
-        }
-        int length = s.length();
-        for (int i = 0; i < length; i++) {
-            if (prev[cur][i]) {
-                path.push(s.substring(i, cur - i));
-                dfs(s, prev, i, path, result);
-                path.pop();
-            }
-        }
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("cat");
+        set.add("cats");
+        set.add("and");
+        set.add("sand");
+        set.add("dog");
+        System.out.println(new WordBreakII140().wordBreak("catsanddog", set));
     }
 }
