cf5e623f886c6e70b4d4134be4aa692a32a9884f
suncl <suncl@ios.ac.cn>
2016-11-02 14:00:21 +0800

    ADD: 180

---
+++ main/com/sunchenglong/leetcode/RemoveDuplicatesfromSortedArrayII80.java
@@ -0,0 +1,18 @@
+package com.sunchenglong.leetcode;
+
+/**
+ * Created by Chenglong Sun on 2016/11/2.
+ */
+public class RemoveDuplicatesfromSortedArrayII80 {
+    public int removeDuplicates(int[] nums) {
+        if (nums == null) return 0;
+        if (nums.length <= 2) return nums.length;
+        int index = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[index - 2]) {
+                nums[index++] = nums[i];
+            }
+        }
+        return index;
+    }
+}
