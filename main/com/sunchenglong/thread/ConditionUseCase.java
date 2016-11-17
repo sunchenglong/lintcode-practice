85bf2a6245c88aec22f28493a975cbe77fa3309e
suncl <suncl@ios.ac.cn>
2016-11-17 11:41:05 +0800

    ADD: ConditionUseCase

---
+++ main/com/sunchenglong/thread/ConditionUseCase.java
@@ -0,0 +1,31 @@
+package com.sunchenglong.thread;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Created by suncl on 2016/11/17.
+ */
+public class ConditionUseCase {
+    Lock lock = new ReentrantLock();
+    Condition condition = lock.newCondition();
+
+    public void conditionWait() throws InterruptedException {
+        lock.lock();
+        try {
+            condition.await();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void conditionSignal() {
+        lock.lock();
+        try {
+            condition.signal();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
