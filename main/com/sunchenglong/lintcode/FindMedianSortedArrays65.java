8884c61787129e732e6bbe317f0d94c2ec52f216
suncl <suncl@ios.ac.cn>
2016-11-18 19:42:36 +0800

    ADD: 65

---
+++ main/com/sunchenglong/lintcode/FindMedianSortedArrays65.java
@@ -0,0 +1,46 @@
+package com.sunchenglong.lintcode;
+
+import com.sunchenglong.leetcode.InterleavingString97;
+
+/**
+ * Created by suncl on 2016/11/18.
+ */
+public class FindMedianSortedArrays65 {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int len = A.length + B.length;
+        if (len % 2 == 1) {
+            return findKth(A, 0, B, 0, len / 2 + 1);
+        }
+        return (
+                findKth(A, 0, B, 0, len / 2) + findKth(A, 0, B, 0, len / 2 + 1)
+        ) / 2.0;
+    }
+
+    public int findKth(int[] A, int A_start,
+                              int[] B, int B_start,
+                              int k) {
+        if (A_start >= A.length) {
+            return B[B_start + k - 1];
+        }
+        if (B_start >= B.length) {
+            return A[A_start + k - 1];
+        }
+
+        if (k == 1) {
+            return Math.min(A[A_start], B[B_start]);
+        }
+
+        int A_key = A_start + k / 2 - 1 < A.length
+                ? A[A_start + k / 2 - 1]
+                : Integer.MAX_VALUE;
+        int B_key = B_start + k / 2 - 1 < B.length
+                ? B[B_start + k / 2 - 1]
+                : Integer.MAX_VALUE;
+
+        if (A_key < B_key) {
+            return findKth(A, A_start + k / 2, B, B_start, k - k / 2);
+        } else {
+            return findKth(A, A_start, B, B_start + k / 2, k - k / 2);
+        }
+    }
+}
