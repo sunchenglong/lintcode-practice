3394a15078a4434386ba536573554ee98b11aa1d
suncl <suncl@ios.ac.cn>
2016-11-22 12:00:48 +0800

    ADD:384

---
+++ main/com/sunchenglong/lintcode/LengthOfLongestSubstring384.java
@@ -0,0 +1,12 @@
+package com.sunchenglong.lintcode;
+
+/**
+ * Created by suncl on 2016/11/18.
+ */
+
+//TODO: Lintcode 384
+public class LengthOfLongestSubstring384 {
+    public int lengthOfLongestSubstring(String s) {
+        return 0;
+    }
+}
